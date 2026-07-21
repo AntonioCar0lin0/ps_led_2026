e8b9e292308796195ca8b4108d2e240643fd25fe
GabrielVMayerhofer <gabrielmayerhofer@gmail.com>
2026-07-21 15:44:03 -0300

    Adição da análise do 10o e 90o percentil

--- sql/distribuicao.sql
+++ sql/distribuicao.sql
@@ -8,76 +8,89 @@ CREATE OR REPLACE TABLE raw_taxi_zone AS SELECT * FROM read_csv_auto('data/taxi_
 .print 'Estatísticas das colunas numéricas da tabela principal'
 
 -- trip_distance
-select 'trip_distance' as coluna, min(trip_distance) as min, max(trip_distance) as max,round(avg(trip_distance), 2) as media,
-    quantile_cont(trip_distance, 0.25) as q1, quantile_cont(trip_distance, 0.5) as mediana, quantile_cont(trip_distance, 0.75) as q3
+select 'trip_distance' as coluna, min(trip_distance) as min, max(trip_distance) as max,
+    round(avg(trip_distance), 2) as media,
+    round(quantile_cont(trip_distance, 0.1), 2) as p10, quantile_cont(trip_distance, 0.25) as q1,
+    quantile_cont(trip_distance, 0.5) as mediana, 
+    quantile_cont(trip_distance, 0.75) as q3, round(quantile_cont(trip_distance, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- passenger_count
 select 'passenger_count' as coluna, min(passenger_count) as min, max(passenger_count) as max, round(avg(passenger_count), 2) as media,
-    quantile_cont(passenger_count, 0.25) as q1, quantile_cont(passenger_count, 0.5) as mediana, quantile_cont(passenger_count, 0.75) as q3
+    round(quantile_cont(passenger_count, 0.1), 2) as p10, quantile_cont(passenger_count, 0.25) as q1, quantile_cont(passenger_count, 0.5) as mediana, 
+    quantile_cont(passenger_count, 0.75) as q3, round(quantile_cont(passenger_count, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- fare_amount
 select 'fare_amount' as coluna, min(fare_amount) as min, max(fare_amount) as max, round(avg(fare_amount), 2) as media,
-    quantile_cont(fare_amount, 0.25) as q1, quantile_cont(fare_amount, 0.5) as mediana, quantile_cont(fare_amount, 0.75) as q3
+    round(quantile_cont(fare_amount, 0.1), 2) as p10, quantile_cont(fare_amount, 0.25) as q1, quantile_cont(fare_amount, 0.5) as mediana, 
+    quantile_cont(fare_amount, 0.75) as q3, round(quantile_cont(fare_amount, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- extra
 select 'extra' as coluna, min(extra) as min, max(extra) as max, round(avg(extra), 2) as media,
-    quantile_cont(extra, 0.25) as q1, quantile_cont(extra, 0.5) as mediana, quantile_cont(extra, 0.75) as q3
+    round(quantile_cont(extra, 0.1), 2) as p10, quantile_cont(extra, 0.25) as q1, quantile_cont(extra, 0.5) as mediana, 
+    quantile_cont(extra, 0.75) as q3, round(quantile_cont(extra, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- mta_tax
 select 'mta_tax' as coluna, min(mta_tax) as min, max(mta_tax) as max, round(avg(mta_tax), 2) as media,
-    quantile_cont(mta_tax, 0.25) as q1, quantile_cont(mta_tax, 0.5) as mediana, quantile_cont(mta_tax, 0.75) as q3
+    round(quantile_cont(mta_tax, 0.1), 2) as p10, quantile_cont(mta_tax, 0.25) as q1, quantile_cont(mta_tax, 0.5) as mediana, 
+    quantile_cont(mta_tax, 0.75) as q3, round(quantile_cont(mta_tax, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- tip_amount
 select 'tip_amount' as coluna, min(tip_amount) as min, max(tip_amount) as max, round(avg(tip_amount), 2) as media,
-    quantile_cont(tip_amount, 0.25) as q1, quantile_cont(tip_amount, 0.5) as mediana, quantile_cont(tip_amount, 0.75) as q3
+    round(quantile_cont(tip_amount, 0.1), 2) as p10, quantile_cont(tip_amount, 0.25) as q1, quantile_cont(tip_amount, 0.5) as mediana, 
+    quantile_cont(tip_amount, 0.75) as q3, round(quantile_cont(tip_amount, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- tolls_amount
 select 'tolls_amount' as coluna, min(tolls_amount) as min, max(tolls_amount) as max, round(avg(tolls_amount), 2) as media,
-    quantile_cont(tolls_amount, 0.25) as q1, quantile_cont(tolls_amount, 0.5) as mediana, quantile_cont(tolls_amount, 0.75) as q3
+    round(quantile_cont(tolls_amount, 0.1), 2) as p10, quantile_cont(tolls_amount, 0.25) as q1, quantile_cont(tolls_amount, 0.5) as mediana, 
+    quantile_cont(tolls_amount, 0.75) as q3, round(quantile_cont(tolls_amount, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- improvement_surcharge
 select 'improvement_surcharge' as coluna, min(improvement_surcharge) as min, max(improvement_surcharge) as max, round(avg(improvement_surcharge), 2) as media,
-    quantile_cont(improvement_surcharge, 0.25) as q1, quantile_cont(improvement_surcharge, 0.5) as mediana, quantile_cont(improvement_surcharge, 0.75) as q3
+    round(quantile_cont(improvement_surcharge, 0.1), 2) as p10, quantile_cont(improvement_surcharge, 0.25) as q1, quantile_cont(improvement_surcharge, 0.5) as mediana, 
+    quantile_cont(improvement_surcharge, 0.75) as q3, round(quantile_cont(improvement_surcharge, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- total_amount
 select 'total_amount' as coluna, min(total_amount) as min, max(total_amount) as max, round(avg(total_amount), 2) as media,
-    quantile_cont(total_amount, 0.25) as q1, quantile_cont(total_amount, 0.5) as mediana, quantile_cont(total_amount, 0.75) as q3
+    round(quantile_cont(total_amount, 0.1), 2) as p10, quantile_cont(total_amount, 0.25) as q1, quantile_cont(total_amount, 0.5) as mediana, 
+    quantile_cont(total_amount, 0.75) as q3, round(quantile_cont(total_amount, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- congestion_surcharge
 select 'congestion_surcharge' as coluna, min(congestion_surcharge) as min, max(congestion_surcharge) as max, round(avg(congestion_surcharge), 2) as media,
-    quantile_cont(congestion_surcharge, 0.25) as q1, quantile_cont(congestion_surcharge, 0.5) as mediana, quantile_cont(congestion_surcharge, 0.75) as q3
+    round(quantile_cont(congestion_surcharge, 0.1), 2) as p10, quantile_cont(congestion_surcharge, 0.25) as q1, quantile_cont(congestion_surcharge, 0.5) as mediana, 
+    quantile_cont(congestion_surcharge, 0.75) as q3, round(quantile_cont(congestion_surcharge, 0.9), 2) as p90
 from raw_taxi
 
 union all
 
 -- airport_fee
 select 'airport_fee' as coluna, min(airport_fee) as min, max(airport_fee) as max, round(avg(airport_fee), 2) as media,
-    quantile_cont(airport_fee, 0.25) as q1, quantile_cont(airport_fee, 0.5) as mediana, quantile_cont(airport_fee, 0.75) as q3
+    round(quantile_cont(airport_fee, 0.1), 2) as p10, quantile_cont(airport_fee, 0.25) as q1, quantile_cont(airport_fee, 0.5) as mediana, 
+    quantile_cont(airport_fee, 0.75) as q3, round(quantile_cont(airport_fee, 0.9), 2) as p90
 from raw_taxi;
