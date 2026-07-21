221200d9fdfae46038bcb9194300143bc6904652
GabrielVMayerhofer <gabrielmayerhofer@gmail.com>
2026-07-21 13:19:16 -0300

    Estatísticas das colunas numéricas

--- sql/duplicatas.sql
+++ sql/duplicatas.sql
@@ -1,14 +1,8 @@
--- Mostra a tabela principal
-.print '========== TABELA PRINCIPAL =========='
+-- Carrega a tabela principal
 CREATE OR REPLACE TABLE raw_taxi AS SELECT * FROM read_parquet('data/yellow_tripdata_2024-01.parquet');
 
-SELECT * FROM raw_taxi LIMIT 5;
-
--- Mostra a tabela de referência (para traduzir códigos de localização) 
-.print '========== TABELA DE REFERENCIA =========='
+-- Carrega a tabela de referência (para traduzir códigos de localização) 
 CREATE OR REPLACE TABLE raw_taxi_zone AS SELECT * FROM read_csv_auto('data/taxi_zone_lookup.csv');
-
-SELECT * FROM raw_taxi_zone LIMIT 5;
 
 -- Mostra a tabela de duplicatas
 .print 'Tabela Duplicatas'
