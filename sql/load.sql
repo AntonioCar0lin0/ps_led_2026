f37b7c644c3f3b5f51a5ed17e40c3f7bfce19e5b
GabrielVMayerhofer <gabrielmayerhofer@gmail.com>
2026-07-24 13:50:02 -0300

    Update gitignore

--- sql/load.sql
+++ sql/load.sql
@@ -4,5 +4,5 @@ CREATE OR REPLACE TABLE raw_taxi AS SELECT * FROM read_parquet('data/01_raw/yell
 -- Carrega a tabela de referência (para traduzir códigos de localização) 
 CREATE OR REPLACE TABLE raw_taxi_zone AS SELECT * FROM read_csv_auto('data/01_raw/taxi_zone_lookup.csv');
 
--- Cria tabela para ser usada na limpeza
+-- Cria a tabela para ser usada na limpeza
 CREATE TABLE IF NOT EXISTS clean_taxi AS SELECT * FROM raw_taxi;
