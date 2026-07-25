b4d9d23fe9b074560a88e99bf04aa26aa97c8a45
GabrielVMayerhofer <gabrielmayerhofer@gmail.com>
2026-07-25 13:06:21 -0300

    Limpeza de nulos

--- sql/02_limpeza/nulos.sql
+++ sql/02_limpeza/nulos.sql
@@ -1,1 +1,3 @@
--- fernando
+-- deleta da tabela as amostras que contem campos nulos (campos identificados no perfilamento)
+delete from clean_taxi 
+where passenger_count is null or RatecodeID is null or congestion_surcharge is null or airport_fee is null or store_and_fwd_flag is null;
