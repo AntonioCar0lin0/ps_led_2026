d8593d2ad87f6b9f60b9801b9f74d381facf4b5a
GabrielVMayerhofer <gabrielmayerhofer@gmail.com>
2026-07-25 13:21:51 -0300

    feat: criacao da tabela fato

--- sql/clean.sql
+++ sql/clean.sql
@@ -1,1 +1,11 @@
 COPY clean_taxi TO 'data/02_clean/yellow_tripdata_2024-01_clean.parquet' (FORMAT PARQUET);
+
+CREATE TABLE fato_corrida (
+    id_corrida SERIAL PRIMARY KEY,
+    id_vendor INT,
+    id_ratecode INT,
+    id_localizacao INT,
+    id_data INT,
+    id_pagamento INT,
+    id_operacao INT    
+)
